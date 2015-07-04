55d9901a5cc1517ec078c70fae8e07942789a4ea
Václav Makeš <info@makes.cz>
2015-07-04 17:25:45 +0200

    Remove debug mode for Symfony and PHP

--- src/www/index.php
+++ src/www/index.php
@@ -6,18 +6,12 @@ require_once __DIR__.'/../src/OAuth2Demo/Server/Models/UserCredentials.php';
 require_once __DIR__.'/../src/OAuth2Demo/Server/Controllers/Token.php';
 require_once __DIR__.'/../src/OAuth2Demo/Server/Controllers/CheckAuthorize.php';
 
-/** show all errors! */
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 /** set up the silex application object */
 $app = new Silex\Application();
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
-
-$app['debug'] = true;
 
 /** set up routes / controllers */
 $app->mount('/', new OAuth2Demo\Server\Server());
